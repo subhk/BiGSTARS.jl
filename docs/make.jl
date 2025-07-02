6e76bd2bae82ef09c7364556ca2a0e3f0a196f72
Subhajit Kar <subhajitkar19@gmail.com>
2025-07-02 14:44:01 +0300

    updates

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,7 @@ using BiGSTARS
 #####
 
 const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
-const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
+const OUTPUT_DIR   = joinpath(@__DIR__, "src", "literated")
 
 mkpath(OUTPUT_DIR)
 
@@ -22,174 +22,84 @@ examples = [
 @info "OUTPUT_DIR: $OUTPUT_DIR"
 
 for example in examples
-    input_file = joinpath(EXAMPLES_DIR, example)
+    input_file  = joinpath(EXAMPLES_DIR, example)
     output_file = joinpath(OUTPUT_DIR, replace(example, ".jl" => ".md"))
     try
-        Literate.markdown(input_file, output_file; 
-                          documenter=true, 
-                          include_comments=true, 
-                          include_code=true, 
-                          include_output=true)
+        Literate.markdown(
+            input_file,
+            output_file;
+            documenter      = true,
+            include_comments = true,
+            include_code     = true,
+            include_output   = true
+        )
     catch e
         @error "Failed to literate $input_file" exception=(e, catch_backtrace())
         rethrow()
     end
 end
 
-# for example in examples
-#   withenv("GITHUB_REPOSITORY" => "github.com/BiGSTARS/BiGSTARSDocumentation") do
-#     example_filepath = joinpath(EXAMPLES_DIR, example)
-#     withenv("JULIA_DEBUG" => "Literate") do
-#       Literate.markdown(example_filepath, OUTPUT_DIR;
-#                         flavor = Literate.DocumenterFlavor(), execute = true)
-#     end
-#   end
-# end
-
-# for example in examples
-#     input_file = joinpath(EXAMPLES_DIR, example)
-#     output_file = joinpath(OUTPUT_DIR, replace(example, ".jl" => ".md"))
-#     try
-#         Literate.markdown(input_file, output_file; 
-#                           documenter=true, 
-#                           include_comments=true, 
-#                           include_code=true, 
-#                           include_output=true)
-#     catch e
-#         @error "Failed to literate $input_file" exception=(e, catch_backtrace())
-#         rethrow()
-#     end
-# end
-
 #####
 ##### Build and deploy docs
 #####
 
-# format = Documenter.HTML(
-#     title = "BiGSTARS.jl",
-#     authors = "BiGSTARS developers",
-#     repo = ""
-#     )
-
-
 format = Documenter.HTML(
-   collapselevel = 2,
-      prettyurls = get(ENV, "CI", nothing) == "true",
-  size_threshold = 2^21,
-       canonical = "https://github.com/BiGSTARS/BiGSTARSDocumentation/stable/"
+    collapselevel  = 2,
+    prettyurls     = get(ENV, "CI", nothing) == "true",
+    size_threshold = 2^21,
+    canonical      = "https://github.com/BiGSTARS/BiGSTARSDocumentation/stable/"
 )
 
-
-bib_filepath = joinpath(dirname(@__FILE__), "src/references.bib")
-bib = CitationBibliography(bib_filepath, style=:authoryear)
-
+bib_filepath = joinpath(dirname(@__FILE__), "src", "references.bib")
+bib          = CitationBibliography(bib_filepath, style=:authoryear)
 
 @printf "making makedocs... \n"
 
-#makedocs = Documenter.make_docs(
 try
     makedocs(
-        authors = "Subhajit Kar, and contributors",
-        sitename = "BiGSTARS.jl",
-        modules = [BiGSTARS],
-        plugins = [bib],
-        format = format,
-        doctest = true,
-        clean = true,
-    checkdocs = :all,
-        pages = Any[
-                    "Home" => "index.md",
-                    "Installation" => "installation_instructions.md",
-                    "Examples" => [
-                        "Stone1971" =>  Any[
-                            "literated/Stone1971.md"
-                            ],
-                        "rRBC" => Any[
-                            "literated/rRBC.md"
-                            ]
-                    ],
-                    "Modules" => Any[
-                        "modules/Stone1971.md",
-                        "modules/rRBC.md",
-                    ],
-                    "Contributor's guide" => "contributing.md",
-                    "References" => "references.md"
+        authors    = ["Subhajit Kar", "Contributors"],
+        sitename   = "BiGSTARS.jl",
+        modules    = [BiGSTARS],
+        plugins    = [bib],
+        format     = format,
+        doctest    = true,
+        clean      = true,
+        checkdocs  = :all,
+        pages      = Any[
+            "Home"           => "index.md",
+            "Installation"   => "installation_instructions.md",
+            "Examples"       => Any[
+                "Stone1971" => "literated/Stone1971.md",
+                "rRBC"       => "literated/rRBC.md"
+            ],
+            "Modules"        => Any[
+                "Stone1971 API" => "modules/Stone1971.md",
+                "rRBC API"      => "modules/rRBC.md"
+            ],
+            "Contributor's Guide" => "contributing.md",
+            "References"      => "references.md"
         ]
     )
 catch e
-    @error "makedocs failed" exception = (e, catch_backtrace())
+    @error "makedocs failed" exception=(e, catch_backtrace())
     rethrow()
 end
 
-# makedocs(
-#      authors = "Subhajit Kar, and contributors",
-#     sitename = "BiGSTARS.jl",
-#      modules = [BiGSTARS],
-#      plugins = [bib],
-#       format = format,
-#      doctest = true,
-#        clean = true,
-#    checkdocs = :all,
-#     pages = Any[
-#                 "Home" => "index.md",
-#                 "Installation" => "installation_instructions.md",
-#                 "Modules" => Any[
-#                     "modules/Ou1971.md",
-#                     "modules/rRBC.md",
-#                 ],
-#                 "Contributor's guide" => "contributing.md",
-#                 "References" => "references.md"
-#     ]
-# )
+@info "Clean up temporary .jld2 and .nc files created by doctests or literated examples..."
 
-
-@info "Clean up temporary .jld2 and .nc output created by doctests or literated examples..."
-
-
-"""
-    recursive_find(directory, pattern)
-
-Return list of filepaths within `directory` that contains the `pattern::Regex`.
-"""
-recursive_find(directory, pattern) =
+function recursive_find(directory, pattern)
     mapreduce(vcat, walkdir(directory)) do (root, dirs, files)
         joinpath.(root, filter(contains(pattern), files))
     end
+end
 
 files = []
 for pattern in [r"\.jld2", r"\.nc"]
-    global files = vcat(files, recursive_find(@__DIR__, pattern))
+    append!(files, recursive_find(@__DIR__, pattern))
 end
 
 for file in files
-    rm(file)
+    rm(file; force=true)
 end
 
-
-# Replace with below once https://github.com/JuliaDocs/Documenter.jl/pull/2692 is merged and available.
-# deploydocs(
-#     repo = "github.com/BiGSTARS/BiGSTARS.jl",
-#     deploy_repo = "github.com/BiGSTARS/BiGSTARSDocumentation",
-#     devbranch = "main",
-#     forcepush = true,
-#     push_preview = true,
-#     versions = ["stable" => "v^", "dev" => "dev", "v#.#.#"]
-# )
-
-
-if get(ENV, "GITHUB_EVENT_NAME", "") == "pull_request"
-    deploydocs(repo = "github.com/BiGSTARS/BiGSTARS.jl",
-               repo_previews = "github.com/BiGSTARS/BiGSTARSDocumentation",
-               devbranch = "main",
-               forcepush = true,
-               push_preview = true,
-               versions = ["stable" => "v^", "dev" => "dev", "v#.#.#"])
-else
-    repo = "github.com/BiGSTARS/BiGSTARSDocumentation"
-    withenv("GITHUB_REPOSITORY" => repo) do
-        deploydocs(; repo,
-                     devbranch = "main",
-                     forcepush = true,
-                     versions = ["stable" => "v^", "dev" => "dev", "v#.#.#"])
-    end
-end
+# deploydocs setup remains as-is...
