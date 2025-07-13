2314b1c2c2831013d3319a3a1de0cfc6c7ee34e0
Subhajit Kar <subhajitkar19@gmail.com>
2025-07-13 15:42:15 +0300

    Update problem.jl

--- src/problem.jl
+++ src/problem.jl
@@ -23,26 +23,70 @@ function Problem{T<:Real}(params::Params{T},
 end
 
 
-struct TwoDGrid{Ny, Nz, params} 
+struct TwoDGrid{T<:AbstractFloat, Ty, Tm} <: AbstractGrid{T, Ty, Tm}
     "Number of grid points in the y-direction"  
     Ny::Int
     "Number of grid points in the z-direction"
     Nz::Int
-    "The problem parameters"
-    params::Params
+    
+    "domain extent in ``y``"
+    L :: T
+    "domain extent in ``z``"
+    H :: T
+
+    "range with ``y``-grid-points"
+    y :: Ty
+    "range with ``z``-grid-points"
+    z :: Ty
+
+    "Differentiation matrices"
+    Dʸ  :: Tm
+    D²ʸ :: Tm
+    D⁴ʸ :: Tm
+
+    Dᶻ  :: Tm
+    D²ᶻ :: Tm
+    D⁴ᶻ :: Tm
 end
 
 
-function TwoDGrid(Ny, Nz, params) 
-    # Ensure Ny and Nz are positive integers
-    if Ny <= 0 || Nz <= 0
-        throw(ArgumentError("Ny and Nz must be positive integers."))
-    end
+function TwoDGrid(Ny, L, Ny, H, T=Float64) 
+    @assert Ny > 0 && Nz > 0, "Ny and Nz must be positive integers."
 
-    y = LinRange(0, 1, Ny)  # y-coordinates
-    z = LinRange(0, 1, Nz)  # z-coordinates
+    # setup Fourier differentiation matrices  
+    # Fourier in y-direction: y ∈ [0, L)
+    y,  Dʸ  = FourierDiff(Ny, 1)
+    _,  D²ʸ = FourierDiff(Ny, 2)
+    _,  D⁴ʸ = FourierDiff(Ny, 4)
 
-    return TwoDGrid(Ny, Nz)
+    # Transform the domain and derivative operators from [0, 2π) → [0, L)
+    y       = (L/2π)   * y
+    Dʸ      = (2π/L)^1 * Dʸ
+    D²ʸ     = (2π/L)^2 * D²ʸ
+    D⁴ʸ     = (2π/L)^4 * D⁴ʸ
+
+    # Chebyshev in the z-direction
+    z,  Dᶻ  = chebdif(Nz, 1)
+    _,  D²ᶻ = chebdif(Nz, 2)
+    _,  D³ᶻ = chebdif(Nz, 3)
+    _,  D⁴ᶻ = chebdif(Nz, 4)
+
+    # Transform the domain and derivative operators from [-1, 1] → [0, H]
+    z, Dᶻ, D²ᶻ  = chebder_transform(z,  Dᶻ, 
+                                        D²ᶻ, 
+                                        zerotoL_transform, 
+                                        H)
+
+    _, _, D⁴ᶻ  = chebder_transform_ho(z, Dᶻ, 
+                                        D²ᶻ, 
+                                        D³ᶻ, 
+                                        D⁴ᶻ, 
+                                        zerotoL_transform_ho, 
+                                        H)
+
+    return TwoDGrid{T, typeof(y), typeof(Dʸ)}(Ny, Nz, L, H, y, z, 
+                                            Dʸ, D²ʸ, D⁴ʸ,
+                                            Dᶻ, D²ᶻ, D⁴ᶻ)
 end
 
 
